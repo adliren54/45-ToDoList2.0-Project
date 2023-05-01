b87f7778ff1a583177a37abad450edbfff25a296
adliren54 <62419296+adliren54@users.noreply.github.com>
2023-05-01 20:59:32 +0000

    add functions in the ToDoList

--- main.py
+++ main.py
@@ -4,10 +4,65 @@ todo = []
 def add():
   time.sleep(1)
   os.system("clear")
-  
+  name = input("Name> ")
+  date = input("Date> ")
+  priority = input("Priority> ").capitalize()
+  row = [name, date, priority]
+  todo.append(row)
+  print("Added")
+
+def view():
+  time.sleep(1)
+  os.system("clear")
+  options = input("1: ALl\n2: By Priority\n>")
+  if options == "1":
+    for row in todo:
+      for item in row:
+        print(item, end=" | ")
+      print()
+    print()
+  elif options == "2":
+    priority = input("What priority?\n>").capitalize()
+    for row in todo:
+      if priority in row:
+        for item in row:
+          print(item, end = " | ")
+        print()
+    print()
+  time.sleep(1)
+
+def edit():
+  time.sleep(1)
+  os.system("clear")
+  find = input("Name of todo to edit > ")
+  found = False
+  for row in todo:
+    if find in row:
+      found = True
+  if not found:
+    print("Couldn't find that")
+    return
+  for row in todo:
+    if find in row:
+      todo.remove(row)
+  name = input("Name> ")
+  date = input("Date> ")
+  priority = input("Priority> ").capitalize()
+  row = [name, date, priority]
+  todo.append(row)
+  print("Added")
+
+def remove():
+  time.sleep(1)
+  os.system("clear")
+  find = input("Name of todo to remove > ")
+  for row in todo:
+    if find in row:
+      todo.remove(row)
+      
 
 while True:
-  menu = input("1: Add\n2: View\n3: Edit\n4: Remove")
+  menu = input("1: Add\n2: View\n3: Edit\n4: Remove\n")
   if menu == "1":
     add()
   elif menu == "2":
@@ -15,9 +70,10 @@ while True:
   elif menu == "3":
     edit()
   elif menu == "4":
-    remove
+    remove()
   else:
     print("That is not a valid input")
-    time.sleep(1)
-    os.system("clear")
     continue
+
+  time.sleep(1)
+  os.system("clear")
